2384d53c12e1bb003171186ca6feb77dab101e0b
patelneel55 <patelneel55@gmail.com>
2017-12-14 23:03:56 -0600

    Basic functions to turn on/off all LEDs

--- LEDCube/LEDCube.ino
+++ LEDCube/LEDCube.ino
@@ -1,4 +1,6 @@
 // Initialize LEDs to designated pins
+int rows = 4;
+int columns = 16;
 int layer[4] = {A3, A2, A1, A0};
 int cols[16] = {13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, A5, A4};
 
@@ -8,11 +10,11 @@ int del = 250;
 void setup() 
 {
   // Initialize the pins to OUTPUT
-  for(int i = 0;i < layer.length;i++)
+  for(int i = 0;i < rows;i++)
   {
     pinMode(layer[i], OUTPUT);
   }
-  for(int i = 0;i < cols.length;i++)
+  for(int i = 0;i < columns;i++)
   {
     pinMode(cols[i], OUTPUT);
   }
@@ -20,4 +22,32 @@ void setup()
 
 void loop()
 {
+  turnOnAll();
 }
+
+/* Turns on all LEDs*/
+void turnOnAll()
+{
+  for(int i = 0;i < rows;i++)
+  {
+    digitalWrite(layer[i], HIGH);
+  }
+    for(int i = 0;i < columns;i++)
+  {
+    digitalWrite(cols[i], LOW);
+  }
+}
+
+/* Turns off all LEDs*/
+void turnOffAll()
+{
+  for(int i = 0;i < rows;i++)
+  {
+    digitalWrite(layer[i], LOW);
+  }
+    for(int i = 0;i < columns;i++)
+  {
+    digitalWrite(cols[i], HIGH);
+  }
+}
+
