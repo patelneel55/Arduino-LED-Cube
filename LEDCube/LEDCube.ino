da32c0d9caed03b9f72f71bbff30e2c3e0f54ec5
patelneel55 <patelneel55@gmail.com>
2017-12-15 15:44:18 -0600

    Implement cascading effect when displaying letters

--- LEDCube/LEDCube.ino
+++ LEDCube/LEDCube.ino
@@ -5,6 +5,7 @@ int columns = 16;
 int layer[4] = {A3, A2, A1, A0};
 int cols[16] = {13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0, A5, A4};
 
+int x = 0;
 // Standard delay for all transitions
 int del = 250;
 
@@ -25,7 +26,11 @@ void loop()
 {
   ///turnOnAll();
   //delay(del);
-  displayLetter('p');
+  while(x == 0)
+  {
+    displayLetter('y');
+    x = 1;
+  }
 }
 
 /* Turns on all LEDs*/
@@ -59,14 +64,14 @@ void turnOffAll()
 /* 3D Coordinate system to turn on a LED */
 void turnOnLED(int x, int y, int z)
 {
-  digitalWrite(layer[rows - z - 1], HIGH);
+  digitalWrite(layer[z], HIGH);
   digitalWrite(cols[x + (4 * y)], LOW);
 }
 
 /* 3D Coordinate system to turn off a LED*/
 void turnOffLED(int x, int y, int z)
 {
-  digitalWrite(layer[rows - z - 1], LOW);
+  digitalWrite(layer[z], LOW);
   digitalWrite(cols[x + (4 * y)], HIGH);
 }
 
@@ -77,13 +82,6 @@ void displayLetter(char letter)
   switch(letter)
   {
     case 'a':
-      // Turn on all rows
-      for(int i = 0; i<rows; i++)
-      {
-        digitalWrite(layer[i], HIGH);
-      }
-
-      
       for(int i = 0;i < columns;i+=4)
       {
         digitalWrite(cols[i], LOW);
@@ -94,15 +92,20 @@ void displayLetter(char letter)
         digitalWrite(cols[i], LOW);
         digitalWrite(cols[i+8], LOW);
       }
-    break;
-
-    case 'h':
-      // Turn on all rows
+      
+      // Turn on all rows layer by layer
       for(int i = 0; i<rows; i++)
       {
         digitalWrite(layer[i], HIGH);
+        delay(75);
+        if(i != rows - 1 && i != 0)
+        {
+          digitalWrite(layer[i + 1], LOW);
+        }
       }
+    break;
 
+    case 'h':
       for(int i = 0;i < columns;i+=4)
       {
         digitalWrite(cols[i], LOW);
@@ -113,16 +116,20 @@ void displayLetter(char letter)
         digitalWrite(cols[i], LOW);
         digitalWrite(cols[i+4], LOW);
       }
-    break;
 
-    case 'p':
-      // Turn on all rows
+      // Turn on all rows layer by layer
       for(int i = 0; i<rows; i++)
       {
         digitalWrite(layer[i], HIGH);
-        turnOnLED(3, 2, i);
+        delay(75);
+        if(i != rows - 1 && i != 0)
+        {
+          digitalWrite(layer[i + 1], LOW);
+        }
       }
+    break;
 
+    case 'p':
       for(int i = 0;i < columns;i+=4)
       {
         digitalWrite(cols[i], LOW);
@@ -132,9 +139,47 @@ void displayLetter(char letter)
         digitalWrite(cols[i], LOW);
         digitalWrite(cols[i+8], LOW);
       }
+      
+      for(int i = rows - 1; i >= 0;i--)
+      {
+        digitalWrite(layer[i], HIGH);
+        turnOnLED(3, 2, i);
+        delay(75);
+        if(i != rows - 1 && i != 0)
+        {
+          digitalWrite(layer[i + 1], LOW);
+          turnOffLED(3, 2, i);
+        }
+      }
+    break;
+
+    case 'y':
+      int c = 0;
+      int m = 0;
+      for(int i = 12;i >= 0;i -= 3)
+      {
+        digitalWrite(cols[i - m], LOW);
+        digitalWrite(cols[i + 3 - c + m], LOW);
+        c++;
+        if(c == 3)
+        {
+          m = 1;
+        }
+      }
+
+      for(int i = 0; i<rows; i++)
+      {
+        digitalWrite(layer[i], HIGH);
+        delay(75);
+        if(i != rows - 1 && i != 0)
+        {
+          digitalWrite(layer[i + 1], LOW);
+        }
+      }
     break;
     
   }
+  delay(del);
 }
 
 
