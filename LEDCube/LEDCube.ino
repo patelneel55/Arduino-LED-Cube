daff1a4076a2b3c56345f15c80c820c68ddf7912
patelneel55 <patelneel55@gmail.com>
2017-12-14 23:25:15 -0600

    Implement 3D coordinate system algorithm to turn on individual LEDs

--- LEDCube/LEDCube.ino
+++ LEDCube/LEDCube.ino
@@ -2,7 +2,7 @@
 int rows = 4;
 int columns = 16;
 int layer[4] = {A3, A2, A1, A0};
-int cols[16] = {13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, A5, A4};
+int cols[16] = {13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0, A5, A4};
 
 // Standard delay for all transitions
 int del = 250;
@@ -28,26 +28,41 @@ void loop()
 /* Turns on all LEDs*/
 void turnOnAll()
 {
-  for(int i = 0;i < rows;i++)
+  for(int x = 0;x < rows;x++)
   {
-    digitalWrite(layer[i], HIGH);
-  }
-    for(int i = 0;i < columns;i++)
-  {
-    digitalWrite(cols[i], LOW);
+    for(int y = 0;y < rows;y++)
+    {
+      for(int z = 0;z < rows;z++)
+      {
+        turnOnLED(x, y, z);
+      }
+    }
   }
 }
 
 /* Turns off all LEDs*/
 void turnOffAll()
 {
+  // Sets all the rows to power source
   for(int i = 0;i < rows;i++)
   {
     digitalWrite(layer[i], LOW);
   }
-    for(int i = 0;i < columns;i++)
+
+  // Sets all the cols to ground
+  for(int i = 0;i < columns;i++)
   {
     digitalWrite(cols[i], HIGH);
   }
 }
+
+/* 3D Coordinate system to turn on a LED */
+void turnOnLED(int x, int y, int z)
+{
+  digitalWrite(layer[rows - z - 1], HIGH);
+  digitalWrite(cols[x + (4 * y)], LOW);
+}
+
+
+
 
